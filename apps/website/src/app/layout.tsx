f504c4e9cdc4000224fd564006d2aadb2832db9b
Trust Onyemachi <85189857+code-with-onye@users.noreply.github.com>
2025-02-07 00:06:44 +0100

    update

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-// import { TourbitProvider } from "@tourbit/cli";
+
 import "./globals.css";
 import { TanstackProvider } from "@/providers/tanstack-provider";
 import { Toaster } from "@/components/ui/sonner";
-import { getCurrentUser } from "@/lib/currentUser";
-import { poppins } from "@/fonts";
+// import { getCurrentUser } from "@/lib/currentUser";
+// import { poppins } from "@/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,31 +16,31 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const user = await getCurrentUser();
+  // const user = await getCurrentUser();
 
-  // console.log(user)
-
-  if (!user) {
-    return null;
-  }
+  // if (!user) {
+  //   return null;
+  // }
 
   return (
     <html lang="en">
-      <body className={poppins.className}>
+      <body>
         <TanstackProvider>
-          {/* <TourbitProvider
+          {children}
+          <Toaster richColors closeButton position="top-left" />
+        </TanstackProvider>
+      </body>
+    </html>
+  );
+}
+
+/* <TourbitProvider
             apiKey={process.env.NEXT_PUBLIC_TOURBIT_API_KEY as string}
             userId={user.userId as string}
             user={{
               name: user.name,
               email: user.email,
             }}
-          > */}
-            {children}
-            <Toaster richColors closeButton position="top-left" />
-          {/* </TourbitProvider> */}
-        </TanstackProvider>
-      </body>
-    </html>
-  );
-}
+          > */
+
+/* </TourbitProvider> */
